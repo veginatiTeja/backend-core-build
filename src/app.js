c97c2df04d5fe1fd647b58770ea60429a0ad5759
root <root@tejaveginati.>
2026-03-17 17:09:56 +0000

    working on moving wallet apis to docker containersation with multi-container applications

--- src/app.js
+++ src/app.js
@@ -10,14 +10,22 @@ const queueDashboard = require('./config/queueDashboard');
 
 app.use(express.json());
 
+app.use(apiLimiter);
+
 app.use('/health', healthRoute);
 app.use('/test', testRoute);
 app.use('/api/users', userRoute);
 app.use('/api/wallet', walletRoute)
-app.use(errorMiddleware);
-app.use(apiLimiter);
+
 
 app.use('/admin/queues', queueDashboard.getRouter());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Wallet API running",
+    service: "backend-core-rebuild"
+  });
+});
 
 //centralized error middle ware
 app.use((err, req, res, next) => {
@@ -28,5 +36,8 @@ app.use((err, req, res, next) => {
     message: err.message || "Internal Server Error"
   });
 });
+
+app.use(errorMiddleware);
+
 
 module.exports = app;
