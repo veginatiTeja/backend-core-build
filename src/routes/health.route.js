f3591724ffdcbb6504c5c814a132d6556a945a78
veginatiTeja <tveginati@gmail.com>
2026-04-05 16:31:19 +0000

    working on git changes using sshe keys

--- src/routes/health.route.js
+++ src/routes/health.route.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const healthController = require('../controllers/health.controller.js');
 
 
-
 router.get('/health', healthController.getHealth);
 router.get('/ready', healthController.getReady);
 
