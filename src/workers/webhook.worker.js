b00fbf965b9b0d9ae0f56b3483de82fd3b6ff2f0
veginatiTeja <tveginati@gmail.com>
2026-04-19 10:17:17 +0000

    fixing the webhook worker processer url issues

--- src/workers/webhook.worker.js
+++ src/workers/webhook.worker.js
@@ -28,7 +28,7 @@ const worker = new Worker("webhookQueue", async (job) => {
             .digest("hex");
 
         const response = await axios.post(
-            "http://webhook-receiver:4000/webhook-receiver",
+           `${process.env.WEBHOOK_RECEIVER_URL}`,
             payload,
             {
                 headers: {
